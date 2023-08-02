d52fdfd494ffdd8716e0d78b7be7aa20ed15cab7
Tenjohok <akahtenjohokwen@gmail.com>
2023-08-02 02:23:45 +0000

    my helloworld script

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                 Helloworld
+# Author:                       Akah 
+# Date of latest revision:      07/31/2023
+# Purpose:                      Create a script that prints a string to the console.
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+echo "hello world"
+
+
+# End
